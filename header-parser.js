253279417180c419b3aa90c08cfd60db4844fd81
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-03-25 12:29:49 -0400

    serve ip address

--- header-parser.js
+++ header-parser.js
@@ -5,9 +5,11 @@ const app = express();
 const begin = ( char ) => { return char === "(" };
 const end = ( char ) => { return char === ")" };
 
+app.enable( 'trust proxy' );
+
 app.get( "/" , ( request, response ) => {
 
-  const IP = request.connection.remoteAddress;
+  const IP = request.ip;
   const userAgent = request.headers["user-agent"].split("");
 
   const begin_index = userAgent.findIndex( begin );
@@ -17,11 +19,11 @@ app.get( "/" , ( request, response ) => {
   const language = request.headers["accept-language"].split(",")[0];
   const software = userAgent.slice( begin_index + 1, end_index ).join("");
 
-  console.log( IP );
+  console.log( request.ip );
 
 
   response.send( JSON.stringify({
-    "ipaddress" : ipaddress,
+    "ipaddress" : IP,
     "language" : language,
     "software" : software
   }));
