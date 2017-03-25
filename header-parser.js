452f35bc1ed6180ea024e35f0424a64f5226028d
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-03-25 10:53:30 -0400

    Initial Commit

---
+++ header-parser.js
@@ -0,0 +1,29 @@
+const express = require( 'express' );
+const app = express();
+const port = process.env.PORT || 3000;
+
+const begin = ( char ) => { return char === "(" };
+const end = ( char ) => { return char === ")" };
+
+app.get( "/" , ( request, response ) => {
+
+  const IP = request.connection.remoteAddress;
+  const userAgent = request.headers["user-agent"].split("");
+
+  const begin_index = userAgent.findIndex( begin );
+  const end_index = userAgent.findIndex( end );
+
+  const ipaddress = request.headers.host;
+  const language = request.headers["accept-language"].split(",")[0];
+  const software = userAgent.slice( begin_index + 1, end_index ).join("");
+
+
+  response.send( JSON.stringify({
+    "ipaddress" : ipaddress,
+    "language" : language,
+    "software" : software
+  }));
+
+}).listen( port, () => {
+  console.log( "listening on " + port );
+});
